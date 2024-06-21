8c7b518af2b100f983bc14cecb50e8e1e395cca3
Edson Silva <edson.junior@ufpi.edu.br>
2024-06-21 13:56:47 -0300

    Chess layer and printing the board/ dimensões&&impressão-tabuleiro

--- src/App/Prog.java
+++ src/App/Prog.java
@@ -1,15 +1,16 @@
 package App;
 
-import boardgame.Position;
+import boardgame.Board;
+import chess.ChessMatch;
+
 
 public class Prog {
 
 	public static void main(String[] args) {
 	
-		Position pos = new Position(4, 7);
-		
-		System.out.println(pos);
-
+		Board board = new Board(8,8);
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }

dec28d9a8f9dee0ec4612f35598744f1af7661a0
Edson Silva <edson.junior@ufpi.edu.br>
2024-06-21 10:45:01 -0300

    primeira classe, criada classe position

---
+++ src/App/Prog.java
@@ -0,0 +1,15 @@
+package App;
+
+import boardgame.Position;
+
+public class Prog {
+
+	public static void main(String[] args) {
+	
+		Position pos = new Position(4, 7);
+		
+		System.out.println(pos);
+
+	}
+
+}
